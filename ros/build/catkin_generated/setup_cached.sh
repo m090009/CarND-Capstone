6c50a19527059c29faf0f51f5877b22a4e31299b
Vern Francisco <avernethy@gmail.com>
2019-03-03 07:54:56 -0800

    changes from catkin_make

--- ros/build/catkin_generated/setup_cached.sh
+++ ros/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/avernethy/sdcNanoTerm3/groupWork/CarND-Capstone/ros/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/avernethy/sdcNanoTerm3/groupWork/CarND-Capstone/ros/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/avernethy/sdcNanoTerm3/groupWork/CarND-Capstone/ros/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/avernethy/sdcNanoTerm3/groupWork/CarND-Capstone/ros/src:$ROS_PACKAGE_PATH"
