3245c3f51d84fafe303cd090ca32702d6c12dfc8
Vern Francisco <avernethy@gmail.com>
2019-03-03 08:39:06 -0800

    remove tracked folders build and devel

--- ros/build/waypoint_loader/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/avernethy/sdcNanoTerm3/groupWork/CarND-Capstone/ros/src/waypoint_loader
-# Build directory: /home/avernethy/sdcNanoTerm3/groupWork/CarND-Capstone/ros/build/waypoint_loader
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
