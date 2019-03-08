de585d632dd4fa6c768dcf5794c493897fa32112
Vern Francisco <avernethy@gmail.com>
2019-03-07 23:11:33 -0800

    removed ignored files

--- ros/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gmock
-# Build directory: /home/avernethy/sdcNanoTerm3/groupWork/CarND-Capstone/ros/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
