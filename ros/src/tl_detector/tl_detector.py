4cebe0db8caf03b277e0ab6c18d5de6a3f802945
m090009 <m090009@gmail.com>
2019-03-20 07:44:38 +0400

    Add prediction models \nAdd prediction logic\nAdd prediction preprocessing

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -9,9 +9,15 @@ from cv_bridge import CvBridge
 from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
+import PIL
 import yaml
 import numpy as np
 from scipy import spatial
+
+# Tensorflow imports 
+import tensorflow as tflow
+from tensorflow.python.platform import gfile
+
 
 STATE_COUNT_THRESHOLD = 3
 
@@ -25,6 +31,7 @@ class TLDetector(object):
         self.waypoint_tree = None
         self.lights = []
 
+
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
@@ -36,10 +43,15 @@ class TLDetector(object):
         rely on the position of the light and the camera image to predict it.
         '''
         sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
+        # NOTE: image_raw
         sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
 
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
+        # Checking to see if the system is on site or in simulation
+        self.is_site = self.config['is_site']
+        # Init model 
+        self.init_model()
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
@@ -53,6 +65,84 @@ class TLDetector(object):
         self.state_count = 0
 
         rospy.spin()
+
+    def init_model(self):
+        print('Loading model ....')
+        # Trained model classes
+        self.classes = ['Green',
+        'Red',
+        'Yellow',
+        'GreenLeft',
+        'RedLeft',
+        'RedRight',
+        'RedStraight',
+        'RedStraightLeft',
+        'GreenRight',
+        'GreenStraightRight',
+        'GreenStraightLeft',
+        'GreenStraight',
+        'off']
+        # Models paths
+        models_path = "../../../models/"
+        sim_model_name = "sim-FL-2400.pb"
+        site_model_name = "site-FL.pb"
+        # Model path, by default its going to be the sim model path
+        model_path = models_path + sim_model_name 
+        self.classes = self.classes[:3]
+        if self.is_site:
+            # model_graph = 
+            # Switch to the model site model path
+            model_path = models_path + site_model_name
+        # Init tensorflow variables 
+        self.tf_graph = self.load_tf_graph(model_path)
+        self.tf_sess = tflow.InteractiveSession() 
+        # Set session's graph (load model)
+        self.tf_sess.graph.as_default()
+        tflow.import_graph_def(self.tf_graph, name='')
+
+        self.input_image = self.tf_sess.graph.get_tensor_by_name("0:0")
+        self.outputs1 = self.tf_sess.graph.get_tensor_by_name("concat_52:0")
+        self.outputs2 = self.tf_sess.graph.get_tensor_by_name("concat_53:0")
+        print("Model loaded \\0/")
+    
+    def load_tf_graph(self, graph_path):
+        print('load graph {}'.format(graph_path))
+        with gfile.FastGFile(graph_path, 'rb') as f:
+            graph_def = tflow.GraphDef()
+        
+        graph_def.ParseFromString(f.read())
+
+        return  graph_def
+
+
+    def normalize_image(self, image):
+        imagenet_stats = (np.array([0.485, 0.456, 0.406]),
+                          np.array([0.229, 0.224, 0.225]))
+        mean = imagenet_stats[0]
+        std = imagenet_stats[1]
+
+        for i in range(image.shape[0]):
+            image[i] = (image[i] - mean[i]) / std[i]
+        return image 
+
+    def resize_image(self, image, w, h):
+        image = cv2.resize(image, (w, h), cv2.INTER_LINEAR).transpose(2, 0, 1)
+        return image
+
+    def preprocess_image(self, image):
+        # Converting the image into rgb
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Resize image 
+        image = self.resize_image(image, 224, 224)
+        # Normalize the image
+        image = self.normalize(image)
+        return image
+
+    # def postprocess_prediction(self):
+
+    def load_model_with_path(self, model_path):
+        return 0
+
 
     def pose_cb(self, msg):
         self.pose = msg
@@ -123,15 +213,24 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        # if(not self.has_image):
-        #     self.prev_light_loc = None
-        #     return False
 
-        # cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        # If we don't get an image we return nothing
+        if(not self.has_image):
+            print("AHAAA")
+            self.prev_light_loc = None
+            return False
 
-        # #Get classification
+
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        print(cv_image.shape)
+        # image = self.preprocess_image(cv_image)
+
+        #Get classification
         # return self.light_classifier.get_classification(cv_image)
         #print(light.state)
+        # prediction = self.tf_sess.run([self.outputs1, self.outputs2],
+        #                      feed_dict={self.input_image: np.expand_dims(image, axis=0)})
+        # print(prediction)
         return light.state
 
     def process_traffic_lights(self):
@@ -147,6 +246,7 @@ class TLDetector(object):
         closest_light = None
         # Traffic light line waypoint
         tl_line_wp_index = None
+        print('HERE')
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
